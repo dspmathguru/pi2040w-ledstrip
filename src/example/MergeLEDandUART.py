479fb0eceb013f722edb1e18c664bf7c7893dd16
Richard Tobias <dspmathguru@gmail.com>
2024-11-04 23:01:01 -0600

    [rjjt] test ideas to figure out how to proceed with server

--- src/example/MergeLEDandUART.py
+++ src/example/MergeLEDandUART.py
@@ -1,33 +1,23 @@
-# Code from https://github.com/pimoroni/pimoroni-pico/blob/main/micropython/examples/plasma2040/alternating-blinkies.py
-# Assumes use of https://shop.pimoroni.com/products/10m-addressable-rgb-led-star-wire
-
-# This super simple example sets up two alternating colours, great for festive lights!
-
+#
 import plasma
 from plasma import plasma2040
 import time
+import os
+import machine
 
-# Set how many LEDs you have
+uart = machine.UART(0, 115200)
+print(uart)
+
+b = None
+msg = ""
+
 NUM_LEDS = 35
-
-# Pick two hues from the colour wheel (from 0-360°, try https://www.cssscript.com/demo/hsv-hsl-color-wheel-picker-reinvented/ )
 HUE_1 = 10
 HUE_2 = 283.7
-
-# Set up brightness (between 0 and 1)
 BRIGHTNESS = 0.5
-
-# Set up speed (wait time between colour changes, in seconds)
-SPEED = 1
+SPEED = 0.5
 
 SATURATION = 1.0
-
-# Pick *one* LED type by uncommenting the relevant line below:
-
-# APA102 / DotStar™ LEDs
-# led_strip = plasma.APA102(NUM_LEDS, 0, 0, plasma2040.DAT, plasma2040.CLK)
-
-# WS2812 / NeoPixel™ LEDs
 led_strip = plasma.WS2812(NUM_LEDS, 0, 0, plasma2040.DAT)
 
 # Start updating the LED strip
@@ -48,3 +38,15 @@ while True:
         else:
             led_strip.set_hsv(i, HUE_1 / 360, SATURATION, BRIGHTNESS)
     time.sleep(SPEED)
+    if uart.any():
+      b = uart.readline()
+      print(type(b))
+      print(b)
+      try:
+        msg = b.decode('utf-8')
+        print(type(msg))
+        print(">> " + msg)
+      except:
+        pass
+
+    
