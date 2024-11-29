f93c7defa724f0618b8d14d0c87d886bb0985d11
Richard J. Tobias <dspmathguru@gmail.com>
2024-11-29 13:17:12 -0600

    [rjjt] updated LedServer

--- src/led-slave/LedServer.py
+++ src/led-slave/LedServer.py
@@ -5,33 +5,33 @@ from machine import UART
 from LedStrip import LedStrip
 
 # Serial setup for communication
-uart = UART(0, baudrate=9600, tx=Pin(0), rx=Pin(1))
-uart.init(9600, bits=8, parity=None, stop=1)
+uart = UART(0, 115200)
 
 i = 1 # 1 second
 mS = 1000
 interval = int(i * mS)
 on = False
+sleepTime = 10 # mS
 
 # Buffer for incoming data
 received_data = ""
 
 def send_json_packet(data):
-    json_data = ujson.dumps(data)
-    uart.write(json_data + "\n")  # Adding newline for easier parsing
+  json_data = ujson.dumps(data)
+  uart.write(json_data + "\n")  # Adding newline for easier parsing
 
 def read_json_packet():
-    global received_data
-    if uart.any():
-        received_data += uart.read().decode()
-        if "\n" in received_data:
-            line, received_data = received_data.split("\n", 1)
-            try:
-                uart.write("ACK\n")
-                return ujson.loads(line)
-            except ValueError:
-                uart.write("ERR: Error parsing JSON:" + str(line) + "\n")
-    return None
+  global received_data
+  if uart.any():
+    received_data += uart.read().decode()
+    if "\n" in received_data:
+      line, received_data = received_data.split("\n", 1)
+      try:
+        uart.write("ACK\n")
+        return ujson.loads(line)
+      except ValueError:
+        uart.write("ERR: Error parsing JSON:" + str(line) + "\n")
+  return None
 
 def doLED(ls):
   if on:
@@ -48,20 +48,20 @@ def main_loop():
     ls = LedStrip()
     
     while True:
-        # Check for incoming JSON data
-        packet = read_json_packet()
-        if packet:
-            parseJSON(packet)
+      # Check for incoming JSON data
+      packet = read_json_packet()
+      if packet:
+        parseJSON(packet)
             
-        # Execute tasks periodically
-        current_time = utime.ticks_ms()
-        tick_time = utime.ticks_diff(current_time, last_task1)
-        if tick_time > interval:
-            task1()
-            last_task1 = current_time
-
-        # Add a small delay to reduce CPU load, adjust as necessary
-        utime.sleep_ms(100)
+      # Execute tasks periodically
+      current_time = utime.ticks_ms()
+      tick_time = utime.ticks_diff(current_time, last_task1)
+      if tick_time > interval:
+        task1()
+        last_task1 = current_time
+        
+      # Add a small delay to reduce CPU load, adjust as necessary
+      utime.sleep_ms(sleepTime)
 
 if __name__ == "__main__":
     main_loop()
