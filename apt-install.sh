efe621cee488f3cf9890f7fced4173aec33dde37
Richard J. Tobias <dspmathguru@gmail.com>
2024-12-01 22:57:34 -0600

    [rjjt] Rasp OS install script added

---
+++ apt-install.sh
@@ -0,0 +1,23 @@
+#! /usr/bin/env bash
+
+sudo apt install -y direnv libusb-1.0-0-dev python3-venv
+sudo apt install -y zsh
+
+if [ ! -d ~/.oh-my-zsh ]; then
+  sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+  cat <<'EOF' >> ~/.zshrc
+
+### needs to be the last thing in the .bashrc file ###
+### adds direnv ###
+if [ -x "$(command -v direnv)" ]; then
+  eval "$(direnv hook $SHELL)"
+fi
+
+EOF
+
+else
+  echo '-----------------------------------------'
+  echo 'Oh My Zsh and direnv already installed'
+  echo '-----------------------------------------'
+fi
+
